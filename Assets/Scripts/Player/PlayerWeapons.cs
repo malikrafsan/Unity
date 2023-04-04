73d2c565fd84defa9a46532706c46d1af0d3c008
Malik Rafsanjani <pro.malikakbar2357@gmail.com>
2023-04-04 17:06:50 +0700

    feat: enable switching weapon using keyboard and mouse

---
+++ Assets/Scripts/Player/PlayerWeapons.cs
@@ -0,0 +1,101 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerWeapons : MonoBehaviour
+{
+    GameObject[][] weapons = new GameObject[4][];
+    int idxWeapon = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (var i = 0; i < weapons.Length; i++)
+        {
+            weapons[i] = new GameObject[0];
+        }
+
+        weapons[0] = new GameObject[2];
+        weapons[0][0] = GameObject.Find("Gun");
+        weapons[0][1] = GameObject.Find("GunBarrelEnd");
+        weapons[1] = new GameObject[1];
+        weapons[1][0] = GameObject.Find("Sword");
+
+        SelectWeapon(idxWeapon);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (var i = 0; i < weapons.Length; i++)
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha1 + i))
+            {
+                idxWeapon = i;
+                SelectWeapon(idxWeapon);
+                break;
+            }
+        }
+
+        if (Input.GetAxisRaw("Mouse ScrollWheel") > 0f)
+        {
+            IncrementIdxWeapon();
+            SelectWeapon(idxWeapon);
+        }
+        else if (Input.GetAxisRaw("Mouse ScrollWheel") < 0f)
+        {
+            DecrementIdxWeapon();
+            SelectWeapon(idxWeapon);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            IncrementIdxWeapon();
+            SelectWeapon(idxWeapon);
+        }
+        else if (Input.GetKeyDown(KeyCode.E))
+        {
+            DecrementIdxWeapon();
+            SelectWeapon(idxWeapon);
+        }
+    }
+
+    private void IncrementIdxWeapon()
+    {
+        idxWeapon++;
+        idxWeapon %= weapons.Length;
+    }
+
+    private void DecrementIdxWeapon()
+    {
+        idxWeapon--;
+        idxWeapon = (idxWeapon + weapons.Length) % weapons.Length;
+    }
+
+    private void SelectWeapon(int idx)
+    {
+        Debug.Log("SelectWeapon: " + idx);
+        DisableAllWeapons();
+        EnableWeapon(idx);
+    }
+
+    private void EnableWeapon(int idx)
+    {
+        int idxUsed = idx % weapons.Length;
+        for (var i = 0; i < weapons[idxUsed].Length; i++)
+        {
+            weapons[idxUsed][i].SetActive(true);
+        }
+    }
+
+    private void DisableAllWeapons()
+    {
+        for (var i = 0; i < weapons.Length; i++)
+        {
+            for (var j = 0; j < weapons[i].Length; j++)
+            {
+                weapons[i][j].SetActive(false);
+            }
+        }
+    }
+}
