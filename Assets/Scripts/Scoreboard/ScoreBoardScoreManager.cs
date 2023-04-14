b77c993179135c5cfddaf5cdde132963b1eaa218
ReinaldoAntolis <reinaldoantolis@gmail.com>
2023-04-14 21:55:58 +0700

    feat: scoreboard

---
+++ Assets/Scripts/Scoreboard/ScoreBoardScoreManager.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+
+public class ScoreBoardScoreManager : MonoBehaviour
+{
+    private ScoreData sd;
+    
+    void Awake()
+    {
+        var json = PlayerPrefs.GetString("scores", "{}");
+        sd = JsonUtility.FromJson<ScoreData>(json);
+    }
+
+    public IEnumerable<Score> GetHighScores()
+    {
+        return sd.scores.OrderBy(x => x.score);
+    }
+
+    public void AddScore(Score score)
+    {
+        sd.scores.Add(score);
+    }
+
+    private void OnDestroy()
+    {
+        SaveScore();
+    }
+
+    public void SaveScore()
+    {
+        var json = JsonUtility.ToJson(sd);
+        PlayerPrefs.SetString("scores", json);
+    }
+}
---
+++ Assets/Scripts/Scoreboard/ScoreUI.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class ScoreUI : MonoBehaviour
+{
+    public RowUi rowUi;
+    public ScoreBoardScoreManager scoreManager;
+
+    void Start()
+    {
+        // scoreManager.AddScore(new Score("hehe", 100));
+        // scoreManager.AddScore(new Score("hhohoho", 1000));
+        //scoreManager.AddScore(new Score("lalalla", 9000));
+
+        var scores = scoreManager.GetHighScores().ToArray();
+        for (int i = 0; i < scores.Length; i ++)
+        {
+            var row = Instantiate(rowUi, transform).GetComponent<RowUi>();
+            row.rank.text = (i + 1).ToString();
+            row.name.text = scores[i].name;
+            row.score.text = scores[i].score.ToString();
+        }
+    }
+}
