e6f83259a61c33a5c29a4d01c3d7815b0e12534e
ReinaldoAntolis <reinaldoantolis@gmail.com>
2023-04-14 17:37:00 +0700

    feat: pet bird and angler (finished)

--- Assets/Scripts/Pet/AnglerAttack.cs
+++ Assets/Scripts/Pet/AnglerAttack.cs
@@ -4,15 +4,107 @@ using UnityEngine;
 
 public class AnglerAttack : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public int damagePerShot = 10;
+    public float timeBetweenBullets = 0.5f;
+    public float range = 100f;
+
+    float timer;
+    Ray shootRay;
+    RaycastHit shootHit;
+    int shootableMask;
+    ParticleSystem gunParticles;
+    LineRenderer gunLine;
+    AudioSource gunAudio;
+    Light gunLight;
+    float effectsDisplayTime = 0.2f;
+
+    private int level = 1;
+    public int Level
     {
-        
+        get
+        {
+            return level;
+        }
+        set
+        {
+            level = value;
+        }
     }
 
-    // Update is called once per frame
+    void Awake()
+    {
+        shootableMask = LayerMask.GetMask("Shootable");
+        gunParticles = GetComponent<ParticleSystem>();
+        gunLine = GetComponent<LineRenderer>();
+        gunAudio = GetComponent<AudioSource>();
+        gunLight = GetComponent<Light>();
+    }
+
     void Update()
     {
-        
+        timer += Time.deltaTime;
+        shootRay.origin = transform.position;
+        shootRay.direction = transform.forward;
+        if (timer >= timeBetweenBullets && !GameControl.control.cantShoot && Physics.Raycast(shootRay, out shootHit, range, shootableMask))
+        {
+            Shoot();
+        }
+
+        if (timer >= timeBetweenBullets * effectsDisplayTime)
+        {
+            DisableEffects();
+        }
+    }
+
+    public void DisableEffects()
+    {
+        gunLine.enabled = false;
+        gunLight.enabled = false;
+    }
+
+    void Shoot()
+    {
+        timer = 0f;
+
+        gunAudio.Play();
+
+        gunLight.enabled = true;
+
+        gunParticles.Stop();
+        gunParticles.Play();
+
+        gunLine.enabled = true;
+        gunLine.SetPosition(0, transform.position);
+
+        shootRay.origin = transform.position;
+        shootRay.direction = transform.forward;
+
+        if (Physics.Raycast(shootRay, out shootHit, range, shootableMask))
+        {
+            EnemyHealth enemyHealth = shootHit.collider.GetComponent<EnemyHealth>();
+            ElementalHealth elementalHealth = shootHit.collider.gameObject.GetComponent<ElementalHealth>();
+
+            if (enemyHealth != null)
+            {
+                enemyHealth.TakeDamage(damagePerShot * level, shootHit.point);
+            }
+
+            if (elementalHealth != null)
+            {
+
+                elementalHealth.TakeDamage(damagePerShot * level, shootHit.point);
+            }
+
+            gunLine.SetPosition(1, shootHit.point);
+        }
+        else
+        {
+            gunLine.SetPosition(1, shootRay.origin + shootRay.direction * range);
+        }
+    }
+
+    public void IncrementLevel()
+    {
+        level++;
     }
 }
--- Assets/Scripts/Pet/PetMovement.cs
+++ Assets/Scripts/Pet/PetMovement.cs
@@ -4,11 +4,19 @@ using UnityEngine;
 
 public class PetMovement : MonoBehaviour
 {
+    public PetType petType;
+    public float timeBetweenBirdAudio = 15f;
+
     GameObject player;
     Transform playerPosition;
     PlayerHealth playerHealth;
+    GameObject enemy;
+    Transform enemyPosition;
+    EnemyHealth enemyHealth;
     PetHealth petHealth;
+    AudioSource birdMoveAudio;
     UnityEngine.AI.NavMeshAgent nav;
+    float timer;
 
     // Start is called before the first frame update
     void Start()
@@ -18,14 +26,42 @@ public class PetMovement : MonoBehaviour
         playerHealth = player.GetComponent <PlayerHealth> ();
         petHealth = GetComponent <PetHealth> ();
         nav = GetComponent <UnityEngine.AI.NavMeshAgent> ();
+
+        if (petType == PetType.Bird)
+        {
+            birdMoveAudio = GetComponent <AudioSource> ();
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        timer += Time.deltaTime;
+        enemy = GameObject.FindGameObjectWithTag("Enemy");
+        if (enemy != null)
+        {
+            enemyPosition = enemy.transform;
+            enemyHealth = enemy.GetComponent <EnemyHealth> ();
+        }
+        
         if (petHealth.currentHealth > 0 && playerHealth.currentHealth > 0)
         {
-            nav.SetDestination (playerPosition.position);
+            if (petType == PetType.Angler && enemy != null)
+            {
+                if (enemyHealth.currentHealth > 0)
+                {
+                    nav.SetDestination (enemyPosition.position);
+                }
+            }
+            else
+            {
+                nav.SetDestination (playerPosition.position);
+                if (petType == PetType.Bird && timer >= timeBetweenBirdAudio)
+                {
+                    timer = 0f;
+                    birdMoveAudio.Play ();
+                }
+            }
         }
         else
         {
